ac74532d5f894a9b406a771866e640923515d407
Charlie McEwen <cmcewen2017@gmail.com>
2024-07-09 22:49:05 -0400

    Updated broken calculator class.

---
+++ matrixCalculatorClasses/Calculator.java
@@ -0,0 +1,44 @@
+package matrixCalculatorClasses;
+import java.util.Scanner;
+
+    
+
+public class Calculator {
+
+
+public static void main(String[] args)
+{
+Scanner scanner = new Scanner(System.in);
+
+int input;
+
+System.out.println("What type of calculation would you like to execute?\n1. Multiply Matrices\n2. Calculate Determinant of Matrix\n3. Calculate Inverse of Matrix\n4. Transpose Matrix");
+input = scanner.nextInt();
+
+switch(input){
+    case 1:
+        MatrixOperations.multiply(scanner);
+        break;
+
+    case 2:
+        Determinant.determinant(scanner);
+        break;
+    
+    case 3:
+        Inverse.inverse(scanner);
+        break;
+
+    case 4:
+        double[][] matrix =  MatrixOperations.transpose(MatrixOperations.matrixInput(scanner));
+        MatrixOperations.transposePrint(matrix);
+        break;
+    default:
+        System.out.println("Error: Invalid Input");
+}
+
+
+scanner.close();
+}
+
+
+}
